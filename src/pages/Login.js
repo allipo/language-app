3a4971c345c5477d8c77c2cb798783f897061bda
allipo <pestotnikall@yahoo.com>
2025-04-20 17:21:46 -0500

    Update API URL to use deployed backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -37,7 +37,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/admin/login', {
+      const response = await fetch('https://allisons-language-app.de.r.appspot.com/api/admin/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
